8176e8ca70ab6b4dd95fee61d8fd586b07b7236d
Srigandhan S V <srigandhan95@gmail.com>
2022-07-01 19:42:41 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-test
+import mlflow
+mlflow.tenorflow.autolog()
+mlflow.set_experiment("Users/srigandhan.v@cdsazure.onmicrosoft.com/MLFLOW_CLI_TEST")
+mlflow.start_run()
+mlflow.log_param("t1","t1")
+mlflow.log_metric("m1",0.33)
+mlflow.end_run()
