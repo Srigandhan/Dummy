ba2a4e67bd4be6b3cb59707909685a0c94402e3a
Srigandhan S V <srigandhan95@gmail.com>
2022-07-01 19:57:43 +0530

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,6 +1,6 @@
 import mlflow
 #mlflow.tensorflow.autolog()
-mlflow.set_experiment("Users/srigandhan.v@cdsazure.onmicrosoft.com/MLFLOW_CLI_TEST")
+mlflow.set_experiment("/Users/srigandhan.v@cdsazure.onmicrosoft.com/MLFLOW_CLI_TEST")
 mlflow.start_run()
 mlflow.log_param("t1","t1")
 mlflow.log_metric("m1",0.33)
